0eb5b427bffa849bec5c3c5096072525e4359f75
Richie000562 <richardbalogh1119@gmail.com>
2025-05-10 09:18:54 +0200

    update basic logic

---
+++ index.js
@@ -0,0 +1,11 @@
+const components = {
+    button : "Search-button",
+    input : "search",
+    container : "pokemon-container"
+
+    
+}
+
+document.getElementById(components.button).addEventListener('click',() => {
+    alert ("keresünk!")
+})
