f5d92e4e67decb2c58cae70824a70888692aaa0a
Richie000562 <richardbalogh1119@gmail.com>
2025-05-10 11:05:57 +0200

    styled

--- index.js
+++ index.js
@@ -1,39 +1,43 @@
 const OPTIONS = {
-    api: 'https://pokeapi.co/api/v2/pokemon/',
-}
+  api: "https://pokeapi.co/api/v2/pokemon/",
+};
 
 const components = {
-    button: "Search-button",
-    input: "search",
-    container: "pokemon-container",
-}
+  button: "Search-button",
+  input: "search",
+  container: "pokemon-container",
+};
 
-document.getElementById(components.button).addEventListener('click', async () => {
-    const data = await getPokemonByName(document.getElementById(components.input).value);
-    renderPokemon(document.getElementById(components.container), data);  
-})
+document
+  .getElementById(components.button)
+  .addEventListener("click", async () => {
+    const data = await getPokemonByName(
+      document.getElementById(components.input).value
+    );
+    renderPokemon(document.getElementById(components.container), data);
+    console.log(data)
+
+  });
 
 async function getPokemonByName(pokemonName) {
-    const response = await fetch(`${OPTIONS.api}${pokemonName}`);
-    const data = await response.json();
-    return data;
+  return (await fetch(`${OPTIONS.api}${pokemonName}`)).json();
 }
 
-
 function renderPokemon(container, data) {
-    const img = document.createElement('img');
-    img.src = data.sprites.front_default;
+  const img = document.createElement("img");
+  img.src = data.sprites.front_default;
 
-    container.innerHTML = ''; 
-    container.appendChild(img); 
+  container.innerHTML = "";
+  container.appendChild(img);
+  container.appendChild(p)
 }
 
 const button = document.getElementById(components.button);
 const input = document.getElementById(components.input);
 
-
 button.disabled = !input.value.trim();
 
-input.addEventListener('input', () => {
-    button.disabled = !input.value.trim();
-})
+input.addEventListener("input", () => {
+  button.disabled = !input.value.trim();
+});
+
