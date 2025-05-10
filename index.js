69d72c190fa6b59e24fcc919d63670eb021fcb4e
Richie000562 <richardbalogh1119@gmail.com>
2025-05-10 10:12:08 +0200

    pokemonfinder

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+const OPTIONS = {
+    api : 'https://pokeapi.co/api/v2/pokemon/'
+}
+
 const components = {
     button : "Search-button",
     input : "search",
@@ -6,6 +10,24 @@ const components = {
     
 }
 
-document.getElementById(components.button).addEventListener('click',() => {
-    alert ("keresünk!")
+document.getElementById(components.button).addEventListener('click',async() => {
+   const data = await getPokemonByName(document.getElementById(components.input).value)
+   createPokemonImage(data)
 })
+
+
+
+async function getPokemonByName(pokemonName) {
+   const response =  await fetch(`${OPTIONS.api}${pokemonName}`)
+   const data = await response.json()
+    return data;
+
+
+}
+
+function createPokemonImage(data){
+    const pokemonImg =data.sprites.front_default
+    const image = document.createElement('img')
+     image.src = pokemonImg
+     document.getElementById(components.container).append(image)
+   }
